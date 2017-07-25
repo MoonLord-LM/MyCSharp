bc55c66849389ffa6011bb2bd83184f3ba4561ac
月之领主LM <178910432@qq.com>
2017-07-25 18:57:30 +0800

    Update

--- FormMain.cs
+++ FormMain.cs
@@ -1,28 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
-using System.Threading;
-using System.Runtime.InteropServices;
 
 namespace MyCSharp
 {
-    /// <summary>
-    /// 
-    /// </summary>
     public partial class FormMain : Form
     {
-        /// <summary>
-        /// 
-        /// </summary>
         public FormMain()
         {
             InitializeComponent();
         }
+
         private void FormMain_Load(object sender, EventArgs e)
         {
+            if (1 - 1 == 0) return;
+            int num = (int)Math.Round( 0 / (double)0 );
+            Application.Exit();
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
         }
+        
+        //2秒
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+        //2秒
+        private void timer2_Tick(object sender,EventArgs e)
+        {
+
+        }
+
     }
 }
